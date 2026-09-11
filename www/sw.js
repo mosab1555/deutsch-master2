520d7b4ca74d6178deeb8922195b838764e019f6
Musab Sobhi <mosabsibhy45@gmail.com>
2026-09-11 04:47:33 -0700

    fix: bump PWA cache so phones receive latest sidebar language update

--- www/sw.js
+++ www/sw.js
@@ -1,5 +1,5 @@
 /* Deutsch Master Academy - offline support (PWA) */
-var DM_CACHE = "german-academy-v11";
+var DM_CACHE = "german-academy-v12";
 var DM_FILES = [
   "./", "./index.html", "./academy.html",
   "./style.css", "./script.js", "./explain.js", "./learn.js", "./play.js", "./world.js", "./study.js", "./life.js", "./mygermany.js", "./dlife.js",
